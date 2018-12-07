b04b48c4e0338fe1453d76e26383bcc54621d875
chenwj <731028571@qq.com>
2018-12-07 11:39:36 +0800

    add: develop

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,82 @@
 <script>
-export default {
-  created () {
-    // 调用API从本地缓存中获取数据
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+  export default {
+    created () {
+      // 调用API从本地缓存中获取数据
+      const logs = wx.getStorageSync('logs') || []
+      logs.unshift(Date.now())
+      wx.setStorageSync('logs', logs)
 
-    console.log('app created and cache logs by setStorageSync')
+      console.log('app created and cache logs by setStorageSync')
+    }
   }
-}
 </script>
 
-<style>
-.container {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  padding: 200rpx 0;
-  box-sizing: border-box;
-}
-/* this rule will be remove */
-* {
-  transition: width 2s;
-  -moz-transition: width 2s;
-  -webkit-transition: width 2s;
-  -o-transition: width 2s;
-}
+<style lang="scss">
+  @import "./styles";
+
+  .container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    padding: 200rpx 0;
+    box-sizing: border-box;
+  }
+
+  div {
+    box-sizing: border-box;
+  }
+
+  /* this rule will be remove */
+  * {
+    transition: width 2s;
+    -moz-transition: width 2s;
+    -webkit-transition: width 2s;
+    -o-transition: width 2s;
+  }
+
+  /* navigator */
+
+  page {
+    height: 100%;
+    overflow: hidden;
+  }
+
+  .wrap {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    .main {
+      width: 100%;
+      height: 100%;
+      padding-bottom: 98rpx;
+      box-sizing: border-box;
+    }
+    .nav {
+      position: absolute;
+      bottom: 0;
+      display: flex;
+      height: 98rpx;
+      width: 100%;
+      z-index: 999;
+      .nav-item {
+        flex-grow: 1;
+        .nav-name {
+          padding-top: 10rpx;
+          margin: 0 auto;
+          color: $vi_base;
+          width: 54rpx;
+          line-height: 28rpx;
+          font-size: 20rpx;
+          text-align: center;
+          img {
+            width: 54rpx;
+            height: 54rpx;
+          }
+        }
+      }
+    }
+  }
+  /* navigator end */
 </style>
--- src/pages/did/index.vue
+++ src/pages/did/index.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="wrap">
     <div class="main">
-      Did
+      <div class="did-todo-nav">
+        <div class="did-toto-nav-item dtni-did" @click="curPage = 1" :class="{active: curPage === 1}">did</div>
+        <div class="did-toto-nav-item dtni-todo" @click="curPage = 2" :class="{active: curPage === 2}">todo</div>
+      </div>
+      <div class="did-wrap" v-if="curPage === 1">
+        <div class="dw-input-wrap">
+          <div class="dwiw-p1">
+            +
+          </div>
+          <div class="dwiw-p2"></div>
+          <div class="dwiw-p3">
+            <input type="text" placeholder="record what u've done today" placeholder-style="color: rgba(255, 255, 255, .5);">
+          </div>
+        </div>
+      </div>
+      <div class="todo-wrap" v-if="curPage === 2">
+        todo
+      </div>
     </div>
     <step-navigator></step-navigator>
   </div>
@@ -10,7 +27,9 @@
   import stepNavigator from '@/components/navigator'
   export default {
     data () {
-      return {}
+      return {
+        curPage: 1
+      }
     },
     components: { stepNavigator },
     mounted () {
@@ -20,6 +39,73 @@
     }
   }
 </script>
-<style>
-
+<style lang="scss">
+  @import "../../styles";
+  .main {
+    display: flex;
+    flex-direction: column;
+    .did-todo-nav {
+      width: 750rpx;
+      height: 106rpx;
+      display: flex;
+      .did-toto-nav-item {
+        text-align: center;
+        width: 90rpx;
+        height: 106rpx;
+        margin: 0 143rpx;
+        color: #666;
+        line-height: 106rpx;
+        &.dtni-did {
+          margin: 0 73rpx 0 212rpx;
+        }
+        &.dtni-todo {
+          margin: 0 212rpx 0 73rpx;
+        }
+        &.active {
+          color: $vi_base;
+          border-bottom: 8rpx solid $vi_base;
+        }
+      }
+    }
+    .did-wrap {
+      flex-grow: 1;
+      position: relative;
+      .dw-input-wrap {
+        height: 94rpx;
+        width: 750rpx;
+        background-color: $vi_base;
+        position: absolute;
+        bottom: 0;
+        padding: 24rpx 24rpx 25rpx 24rpx;
+        display: flex;
+        .dwiw-p1 {
+          width: 35rpx;
+          height: 35rpx;
+          margin: 4rpx 23rpx 4rpx 0;
+          color: #fff;
+          font-size: 60rpx;
+          line-height: 23rpx;
+        }
+        .dwiw-p2 {
+          width: 2rpx;
+          height: 32rpx;
+          background-color: #f0f0f0;
+          opacity: 0.5;
+          margin: 7rpx 0 6rpx 0;
+        }
+        .dwiw-p3 {
+          margin-left: 24rpx;
+          flex-grow: 1;
+          input {
+            height: 45rpx;
+            min-height: 45rpx;
+            color: #fff;
+          }
+        }
+      }
+    }
+    .todo-wrap {
+      flex-grow: 1;
+    }
+  }
 </style>
